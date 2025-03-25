7c34626d49ec99b5d1d37307cd54b1219226cc10
Allan <allan.viana2003@gmail.com>
2025-03-25 20:17:36 -0300

    Initial commit

---
+++ MovieApp-iOS/MovieApp_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp_iOSApp.swift
+//  MovieApp-iOS
+//
+//  Created by Allan Viana on 25/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MovieApp-iOSTests/MovieApp_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp_iOSTests.swift
+//  MovieApp-iOSTests
+//
+//  Created by Allan Viana on 25/03/25.
+//
+
+import Testing
+@testable import MovieApp_iOS
+
+struct MovieApp_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
